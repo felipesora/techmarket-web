a6958bc3b31fcce8b02d36cf79e753e1542a1d35
felipesora <felipeusora@gmail.com>
2026-03-23 14:53:59 -0300

    feat: adicionando cabecalho

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Cabecalho } from './layout/cabecalho/cabecalho';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Cabecalho],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
