09626e0d797b2663fb73844d4cba4f488f361dec
felipesora <felipeusora@gmail.com>
2026-03-23 15:13:06 -0300

    feat: criando navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Cabecalho } from './layout/cabecalho/cabecalho';
+import { Navbar } from './layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Cabecalho],
+  imports: [RouterOutlet, Cabecalho, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
