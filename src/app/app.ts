777b7e0b481818c39e4c169f073aac8998c3b074
felipesora <felipeusora@gmail.com>
2026-03-23 21:42:48 -0300

    feat: começando rodape

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Cabecalho } from './layout/cabecalho/cabecalho';
 import { Navbar } from './layout/navbar/navbar';
+import { Rodape } from "./layout/rodape/rodape";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Cabecalho, Navbar],
+  imports: [RouterOutlet, Cabecalho, Navbar, Rodape],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
