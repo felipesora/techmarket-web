33bbf5bc393dea31b3960f5a59a93315585d6e0f
felipesora <felipeusora@gmail.com>
2026-03-24 16:13:38 -0300

    fix: ajustando os testes

--- src/app/layouts/main-layout/main-layout.spec.ts
+++ src/app/layouts/main-layout/main-layout.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { MainLayout } from './main-layout';
 
 describe('MainLayout', () => {
@@ -8,7 +8,7 @@ describe('MainLayout', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainLayout],
+      imports: [MainLayout, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MainLayout);
