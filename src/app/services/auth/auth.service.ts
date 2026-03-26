fc9bf40566f23240229064217ecd1ded82adc74e
felipesora <felipeusora@gmail.com>
2026-03-26 15:08:30 -0300

    feat: adicionando validação do token

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -18,4 +18,37 @@ export class AuthService {
 
     return decoded;
   }
+
+  logout() {
+    localStorage.removeItem('tokenUser');
+    localStorage.removeItem('idUsuarioLogado');
+    localStorage.removeItem('perfil');
+    localStorage.removeItem('exp');
+  }
+
+  isTokenValid() {
+    const token = localStorage.getItem('tokenUser');
+    if (!token) return false;
+
+    try {
+      const decoded = jwtDecode<TokenPayload>(token);
+      const currentTime = Date.now() / 1000;
+
+      if (decoded.exp < currentTime) {
+        this.logout();
+        return false;
+      }
+
+      return true;
+
+    } catch (error) {
+      console.error("Erro ao decodificar token:", error);
+      this.logout();
+      return false;
+    }
+  }
+
+  isUsuarioLogado(): boolean {
+    return this.isTokenValid();
+  }
 }
