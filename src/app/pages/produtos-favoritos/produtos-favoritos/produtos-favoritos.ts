44914881faec764e9aef961b3b9fb148a2ca3285
felipesora <felipeusora@gmail.com>
2026-03-30 15:48:39 -0300

    feat: página de produtos favoritos

---
+++ src/app/pages/produtos-favoritos/produtos-favoritos/produtos-favoritos.ts
@@ -0,0 +1,46 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Produto } from '../../../types/produto';
+import { ProdutoService } from '../../../services/produto/produto.service';
+import { AuthService } from '../../../services/auth/auth.service';
+import { CardProduto } from "../../../components/card-produto/card-produto";
+
+@Component({
+  selector: 'app-produtos-favoritos',
+  imports: [CardProduto],
+  templateUrl: './produtos-favoritos.html',
+  styleUrl: './produtos-favoritos.css',
+})
+export class ProdutosFavoritos implements OnInit {
+
+  produtosFavoritados: Produto[] = [];
+
+  constructor(
+    private produtoService: ProdutoService,
+    private cdr: ChangeDetectorRef,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.listarProdutosFavoritados();
+  }
+
+  listarProdutosFavoritados() {
+    const idsProdutosFavoritados: string[] = JSON.parse(localStorage.getItem('produtos-favoritos') || '[]');
+
+    if (idsProdutosFavoritados.length === 0) {
+      this.produtosFavoritados = [];
+      return;
+    }
+
+    this.produtoService.getProdutosFavoritados(idsProdutosFavoritados).subscribe({
+      next: (response) => {
+        this.produtosFavoritados = response;
+        this.cdr.detectChanges();
+        console.log('Produtos favoritados: ', this.produtosFavoritados);
+      },
+      error: (error) => {
+        console.error('Erro ao carregar produtos favoritados:', error);
+      }
+    })
+  }
+}
