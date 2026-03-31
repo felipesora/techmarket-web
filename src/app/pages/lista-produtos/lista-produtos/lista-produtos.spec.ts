46dfb8da59275e077cee7178d7ba5d8064604e3d
felipesora <felipeusora@gmail.com>
2026-03-31 16:04:28 -0300

    fix: ajustando testes

--- src/app/pages/lista-produtos/lista-produtos/lista-produtos.spec.ts
+++ src/app/pages/lista-produtos/lista-produtos/lista-produtos.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListaProdutos } from './lista-produtos';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ListaProdutos', () => {
   let component: ListaProdutos;
@@ -8,7 +9,7 @@ describe('ListaProdutos', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ListaProdutos],
+      imports: [ListaProdutos, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ListaProdutos);
